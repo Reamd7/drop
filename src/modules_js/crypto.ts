f585658ea8e7386caa23733f85dd4f78554f3dde
Sepehr Laal <5657848+3p3r@users.noreply.github.com>
2023-02-01 22:00:52 -0800

    chore: pre for release of 3.3.1 with lint/format improvements

--- src/modules_js/crypto.ts
+++ src/modules_js/crypto.ts
@@ -1,3 +1,5 @@
+// @ts-ignore
 import * as crypto from "crypto-browserify";
 export default crypto;
+// @ts-ignore
 export * from "crypto-browserify";
--- src/modules_js/zlib.ts
+++ src/modules_js/zlib.ts
@@ -1,3 +1,5 @@
+// @ts-ignore
 import * as zlib from "browserify-zlib";
+// @ts-ignore
 export * from "browserify-zlib";
 export default zlib;
