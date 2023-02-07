93e72f89c7d8fa1e1469f39788c0715db10aee4a
Sepehr Laal <5657848+3p3r@users.noreply.github.com>
2023-02-06 18:02:57 -0800

    fix: install missing Node global timers

--- src/main.js
+++ src/main.js
@@ -10,4 +10,12 @@
 	const _console = await import("console");
 	globalThis.console = _console;
 })();
+(async () => {
+	const _timers = await import("timers");
+	// setTimeout and clearTimeout are already defined in the global scope
+	globalThis.setImmediate = _timers.setImmediate;
+	globalThis.clearImmediate = _timers.clearImmediate;
+	globalThis.setInterval = _timers.setInterval;
+	globalThis.clearInterval = _timers.clearInterval;
+})();
 globalThis.global = globalThis;
